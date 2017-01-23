43146e9ae7b36b41c58037a50f904d29194357cc
Erodis Pérez Michel <EroPerez@users.noreply.github.com>
2017-01-23 16:15:08 -0500

    Fixing some  bugs
    Fixing calling attributes from idp source:
            $syncroles = $simplesamlphp_authsource->syncroles;
            if ($syncroles) {
                // The roles provided by the IdP.
                $roleMapping = $simplesamlphp_authsource->roleattr;
    
                if (!empty($roleMapping) && isset($attrs[$roleMapping]) && !empty($attrs[$roleMapping])) {
                    $adminsRole = explode(',', $simplesamlphp_authsource->rolemappattr);

--- simplesamlphp_auth.api.php
+++ simplesamlphp_auth.api.php
@@ -16,7 +16,8 @@
  *   The roles that have been selected for the current user
  *   by the role evaluation process, in the format array($rid => $rid)
  */
-function hook_simplesamlphp_auth_user_roles_alter(&$roles) {    
+function hook_simplesamlphp_auth_user_roles_alter(&$roles) {
+    watchdog('simplesamlphp_auth', "Altering user roles");
 
     // Get an instance of SimpleSAML_Drupal
     try {
@@ -27,57 +28,54 @@ function hook_simplesamlphp_auth_user_roles_alter(&$roles) {
         // Get SAML attributes
         $attrs = $simplesamlphp_auth_as->getAttributes();
         $simplesamlphp_authsource = $simplesamlphp_drupal->getAuthSourceAttributes();
-    } catch (Exception $e) {
-        watchdog_exception('simplesamlphp_auth', $e);
-    } // end try - catch
-   
-    $syncroles = $simplesamlphp_drupal->getAttrsFromAssertion($simplesamlphp_authsource->syncroles);
+
+        $syncroles = $simplesamlphp_authsource->syncroles;
 
 
-    if ($syncroles) {
-        // The roles provided by the IdP.
-        $roleMapping = $simplesamlphp_drupal->getAttrsFromAssertion($simplesamlphp_authsource->roleattr);
+        if ($syncroles) {
+            // The roles provided by the IdP.
+            $roleMapping = $simplesamlphp_authsource->roleattr;
 
-        if (!empty($roleMapping) && isset($attrs[$roleMapping]) && !empty($attrs[$roleMapping])) {
-            $adminsRole = explode(',', $simplesamlphp_drupal->getAttrsFromAssertion($simplesamlphp_authsource->rolemappattr));
-            
-            $administrator = user_role_load_by_name('administrator');
-            $adminWeight = $administrator->rid;
-            // Match role names in the saml attributes to local role names.
-            $roleWeight = 0;
-            foreach ($attrs[$roleMapping] as $samlRole) {
-                $samlRole = trim($samlRole);
-                if (empty($samlRole)) {
-                    break;
-                }                
-                else if (in_array($samlRole, $adminsRole)) {
-                    if ($roleWeight < $adminWeight) {
-                        $roleWeight = $adminWeight;
-                    }
-                    break;
-                } else {
-                    if ($loadedRole = user_role_load_by_name($samlRole)) {
-                        $roles[$loadedRole->rid] = $loadedRole->name;
+            if (!empty($roleMapping) && isset($attrs[$roleMapping]) && !empty($attrs[$roleMapping])) {
+                $adminsRole = explode(',', $simplesamlphp_authsource->rolemappattr);
+                $administrator = user_role_load_by_name('administrator');
+                $adminWeight = $administrator->rid;
+                // Match role names in the saml attributes to local role names.
+                $roleWeight = 0;
+                foreach ($attrs[$roleMapping] as $samlRole) {
+                    $samlRole = trim($samlRole);
+                    if (empty($samlRole)) {
+                        break;
+                    } else if (in_array($samlRole, $adminsRole)) {
+                        if ($roleWeight < $adminWeight) {
+                            $roleWeight = $adminWeight;
+                        }
+                        break;
+                    } else {
+                        if ($loadedRole = user_role_load_by_name($samlRole)) {
+                            $roles[$loadedRole->rid] = $loadedRole->name;
+                        }
                     }
                 }
+                switch ($roleWeight) {
+                    // case 5:
+                    //   $roles = array(5 => 'customrole');
+                    //   break;
+                    case $adminWeight:
+                        $roles[$adminWeight] = 'administrator';
+                        break;
+                    case DRUPAL_AUTHENTICATED_RID: // default value => 2
+                    default:
+                        $roles[DRUPAL_AUTHENTICATED_RID] = 'authenticated user';
+                        break;
+                }
             }
-            switch ($roleWeight) {
-                // case 5:
-                //   $roles = array(5 => 'customrole');
-                //   break;
-                case $adminWeight:
-                    $roles[$adminWeight] = 'administrator';
-                    break;
-                case DRUPAL_AUTHENTICATED_RID: // default value => 2
-                default:
-                    $roles[DRUPAL_AUTHENTICATED_RID] = 'authenticated user';
-                    break;
-            }
+        } else {
+            $roles[DRUPAL_AUTHENTICATED_RID] = 'authenticated user';
         }
-    }else {        
-      $roles[DRUPAL_AUTHENTICATED_RID] = 'authenticated user';    
-    }
-
+    } catch (Exception $e) {
+        watchdog_exception('simplesamlphp_auth', $e);
+    } // end try - catch  
     /*
      * global $_simplesamlphp_auth_saml_attributes; 
       if (isset($_simplesamlphp_auth_saml_attributes['roles'])) {
